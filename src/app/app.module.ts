5f219a0fa50e91d2fb382321bab03d232fab3807
matheo <math.sou@hotmail.com>
2022-04-10 15:36:05 -0300

    adicionado estilização com o prime NG e criado header da página

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {CardModule} from 'primeng/card';
+import {InputTextModule} from 'primeng/inputtext'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './views/header/header.component';
+import { UserAddComponent } from './views/user-add/user-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UserAddComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CardModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
