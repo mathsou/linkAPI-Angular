547bd95ec8565ab5c234e48bc1700e5b165cc6d9
matheo <math.sou@hotmail.com>
2022-04-11 17:37:24 -0300

    rota para a tela inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
 import { JobsAddComponent } from './views/jobs-add/jobs-add.component';
 import { JobsListComponent } from './views/jobs-list/jobs-list.component';
 import { UserAddComponent } from './views/user-add/user-add.component';
 import { UsersListComponent } from './views/users-list/users-list.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent},
   { path: 'users', component: UsersListComponent },
   { path: 'users/add', component: UserAddComponent },
   { path: 'users/add/:id', component: UserAddComponent },
